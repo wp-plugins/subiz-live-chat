74694af77edc8cb52c69a895b36c30ef9efb624e
mrsubiz <mrsubiz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-14 07:15:52 +0000

    Update file subiz-for-wp.php
    git-svn-id: https://plugins.svn.wordpress.org/subiz-live-chat/trunk@875256 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- subiz-for-wp.php
+++ subiz-for-wp.php
@@ -24,7 +24,6 @@ if (isset($_POST['subiz_licence_id'])) {
 function sfw_insert() {
     $subiz_licence_id = get_option('subiz_licence_id');
     echo '<script type="text/javascript">window._sbzq||function(e){e._sbzq=[];var t=e._sbzq;t.push(["_setAccount",' . $subiz_licence_id . ']);var n=e.location.protocol=="https:"?"https:":"http:";var r=document.createElement("script");r.type="text/javascript";r.async=true;r.src=n+"//static.subiz.com/public/js/loader.js";var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)}(window);</script>';
-    echo $subiz_licence_id;
 }
 
 function sfw_init() {
